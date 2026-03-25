ff059c3ad9a8f427d09ff11d96c26d19f8a04730
Krishia <krishiaranquebello@gmail.com>
2026-03-25 15:57:35 +0800

    fix: use timezone for ride event created_at and create migrations

--- rides/models/ride_event.py
+++ rides/models/ride_event.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from .ride import Ride
 
 class RideEvent(models.Model):
@@ -17,7 +18,7 @@ class RideEvent(models.Model):
         db_column='id_ride'
     )
     description = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
     
     class Meta:
         db_table = 'ride_event'
